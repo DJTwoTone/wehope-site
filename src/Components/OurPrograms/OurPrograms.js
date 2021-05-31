d97eb7a1622c53dbfd645069e8212b14e5c96d0a
DJTwoTone <34737313+DJTwoTone@users.noreply.github.com>
2021-05-31 16:17:17 +0900

    adding current file structure

---
+++ src/Components/OurPrograms/OurPrograms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OurPrograms() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default OurPrograms
---
+++ src/Pages/BlogEntry/BlogEntry.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BlogEntry() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default BlogEntry
